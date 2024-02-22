060ff31a58ab4f3fb91fce26969ad651ab308bc9
Neeraj Singh <869neerajsingh@gmail.com>
2024-02-22 13:51:26 +0530

    feat: protect cart and orders route

--- src/components/secure/PrivateRoute.js
+++ src/components/secure/PrivateRoute.js
@@ -1,4 +1,11 @@
+import { Navigate } from "react-router-dom";
+import { useUserAuthContextValue } from "../../contexts/userAuthContext";
+
 const PrivateRoute = ({ children }) => {
+    const { isSignedIn } = useUserAuthContextValue();
+
+    if (!isSignedIn) return <Navigate to="/" replace={ true } />;
+
     return (
         children
     )
