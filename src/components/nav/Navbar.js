530b2d2ecf445c97457226f06afd2668bfae10f4
Neeraj Singh <869neerajsingh@gmail.com>
2024-02-22 14:02:01 +0530

    feat: implment basic navbar

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,9 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useUserAuthContextValue } from "../../contexts/userAuthContext";
 
 const Navbar = () => {
+    const { isSignedIn, setIsSignedIn } = useUserAuthContextValue();
+    const navigate = useNavigate();
+
     return (
         <>
             <h2>Navbar Page</h2>
+            <ul>
+                <li>BuyBusy</li>
+                <li>Home</li>
+                { isSignedIn ? (
+                    <>
+                        <li>My Orders</li>
+                        <li>Cart</li>
+                        <li onClick={ () => {
+                            navigate("/");
+                            setIsSignedIn(false);
+                        } }>Logout</li>
+                    </>
+                ) : (
+                    <>
+                        <li>Signin</li>
+                    </>
+                ) }
+            </ul>
             <Outlet />
         </>
     )
