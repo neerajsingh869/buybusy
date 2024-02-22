e22965ef1b17d52b09ac5357828c13bf43832a2b
Neeraj Singh <869neerajsingh@gmail.com>
2024-02-22 13:10:37 +0530

    feat: implement basic register workflow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { app } from "./configs/firebase";
 import { 
   createRoutesFromElements, 
   createBrowserRouter, 
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,34 @@
+import { useRef } from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+
 const Register = () => {
+    const inputEmail = useRef();
+    const inputPassword = useRef();
+
+    const auth = getAuth();
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        try {
+            const email = inputEmail.current.value;
+            const password = inputPassword.current.value;
+
+            await createUserWithEmailAndPassword(auth, email, password);
+            console.log("User signed up successfully!")
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
     return (
         <>
             <h2>Register Page</h2>
+            <form onSubmit={ handleSignUp }>
+                <input type="email" placeholder="Email" ref={ inputEmail } />
+                <input type="password" placeholder="Password" ref={ inputPassword } />
+                <button>Sign Up</button>
+            </form>
         </>
     )
 };
