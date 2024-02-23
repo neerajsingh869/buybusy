f0e76edf65928e2885f95d9ea61456be46f8af48
Neeraj Singh <869neerajsingh@gmail.com>
2024-02-23 14:17:33 +0530

    style: use css to style home page, product page, and add basic data to display on ui

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,8 +1,21 @@
-const ProductCard = () => {
+import styles from "./ProductCard.module.css";
+
+const ProductCard = ({ product, homeOrCart }) => {
     return (
-        <>
-            <h2>ProductCard Page</h2>
-        </>
+        <div key={ product.id } className={ styles.productContainer }>
+            <div className={ styles.productImageContainer }>
+                <img src={ product.image } alt="Product"/>
+            </div>
+            <div className={ styles.productTitleContainer }>
+                { product.title }
+            </div>
+            <div className={ styles.productPriceContainer }>
+                &#8377; { product.price }
+            </div>
+            <button className={ styles.productActionBtn }>
+                { homeOrCart === "home" ? "Add to Cart" : "Remove from Cart" }
+            </button>
+        </div>
     )
 };
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,29 @@
+import ProductCard from "../../components/productCard/ProductCard";
+import productsData from "../../data/data.json";
+import styles from "./Home.module.css";
+
 const Home = () => {
     return (
-        <>
-            <h2>Home Page</h2>
-        </>
+        <div>
+            <form className={ styles.searchForm }>
+                <input type="text" placeholder="Search By Name" />
+            </form>
+            <aside className={ styles.filterAside }>
+                <h2>Filter</h2>
+                <form>
+
+                </form>
+            </aside>
+            <div className={ styles.prductsContainer }>
+                {
+                    productsData.map((product, index) => {
+                        return (
+                            <ProductCard product={ product } homeOrCart="home" />
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 };
 
