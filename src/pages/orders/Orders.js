475b80338243eb609c3878acbe88d6aea00963ab
Neeraj Singh <869neerajsingh@gmail.com>
2024-04-17 23:39:54 +0530

    style: use tailwind to style orders page

--- src/pages/orders/Orders.js
+++ src/pages/orders/Orders.js
@@ -2,7 +2,6 @@ import { DotLoader } from "react-spinners";
 import { useSelector } from "react-redux";
 
 import Order from "../../components/order/Order";
-import styles from "./Orders.module.css";
 import { ordersSelector } from "../../redux/reducers/ordersReducer";
 
 const Orders = () => {
@@ -23,14 +22,14 @@ const Orders = () => {
 
   if (orders.length === 0) {
     return (
-      <div className={styles.emptyOrdersPage}>
+      <div className="m-4 text-xl text-center">
         <h2>No orders found!</h2>
       </div>
     );
   }
 
   return (
-    <div className={styles.ordersContainer}>
+    <div className="p-8 flex justify-start items-center flex-col">
       <h1>Your Orders</h1>
       {orders.map((order) => {
         return <Order key={order.id} order={order} />;
