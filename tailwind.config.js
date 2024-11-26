b5b244aa9acd2fa1c2d3cb328c04df939ec0c6f5
Neeraj Singh <869neerajsingh@gmail.com>
2024-11-26 18:27:31 +0530

    style: add animation to slide an element to left from 100% right to its original place

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-in-left": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+      animation: {
+        "slide-in-left": "slide-in-left 0.5s ease-out forwards",
+      },
+    },
   },
   plugins: [],
-}
-
+};
