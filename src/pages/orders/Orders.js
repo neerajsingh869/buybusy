43f46b82861e82ebb83d782e1c24bbc57226fb05
Neeraj Singh <869neerajsingh@gmail.com>
2024-12-04 15:39:44 +0530

    feat: make my orders tab visible for all users and write condition to navigate users to signin page if they are not signed in or signed up

--- src/pages/orders/Orders.js
+++ src/pages/orders/Orders.js
@@ -1,15 +1,33 @@
+import { useEffect, useRef } from "react";
 import { DotLoader } from "react-spinners";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import Order from "../../components/order/Order";
 import { ordersSelector } from "../../redux/reducers/ordersReducer";
+import { userSelector } from "../../redux/reducers/userReducer";
+import { showNotification } from "../../utility/showNotifications";
 
 const Orders = () => {
   const { orders, loading } = useSelector(ordersSelector);
+  const { userUid } = useSelector(userSelector);
+
+  const navigate = useNavigate();
+  const hasRunEffect = useRef(false);
+
+  useEffect(() => {
+    if (!hasRunEffect.current) {
+      if (!userUid) {
+        showNotification("Please SignIn or SignUp to view your Orders.");
+        navigate("/signin");
+      }
+      hasRunEffect.current = true;
+    }
+  }, [userUid, navigate]);
 
   if (loading) {
     return (
-      <div className="pageLoader">
+      <div className="pageLoader flex-1 flex items-center justify-center">
         <DotLoader
           color="#7064e5"
           size={70}
