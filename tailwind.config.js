c7e814e4420baefeb3f023d20fdad6a926015648
Neeraj Singh <869neerajsingh@gmail.com>
2024-12-11 20:26:25 +0530

    feat: sync tailwind configs from original cra app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "slide-in-left": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+      animation: {
+        "slide-in-left": "slide-in-left 0.5s ease-out forwards",
+      },
+    },
   },
   plugins: [],
 }
