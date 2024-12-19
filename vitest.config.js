12ba78b2ed59bd60304e3f6180715dd65803e8a7
Neeraj Singh <869neerajsingh@gmail.com>
2024-12-19 13:50:10 +0530

    feat: Tell vitest where to look for test files

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,10 @@
 // vitest.config.js
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     globals: true,
-  }
-})
+    include: ["tests/**/*.test.{ts,tsx}"],
+  },
+});
