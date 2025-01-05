890a5d1911074ecbe730297454f23f6b0d2c1227
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-05 20:57:05 +0530

    test: Update and add test cases for Loader component

--- tests/components/Loader.test.tsx
+++ tests/components/Loader.test.tsx
@@ -5,12 +5,24 @@ import "@testing-library/jest-dom";
 
 import Loader from "../../src/components/Loader";
 
-describe("Loader", () => {
-  it("should show dot loader", () => {
+describe("Loader Component", () => {
+  it("should render the loader component with correct props", () => {
     render(<Loader />);
 
     const dotLoader = screen.getByTestId("loader");
 
     expect(dotLoader).toBeInTheDocument();
+    expect(dotLoader).toHaveAttribute("aria-label", "Loading Spinner");
+  });
+
+  it("should be properly centered in the viewport", () => {
+    render(<Loader />);
+
+    const dotLoaderWrapper = screen.getByTestId("loader").parentElement;
+
+    expect(dotLoaderWrapper).toHaveClass(
+      "absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2",
+      { exact: true }
+    );
   });
 });
