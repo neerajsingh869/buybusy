09f06be399b437a8cb6d4463b77e7bee5f97edd4
Neeraj Singh <869neerajsingh@gmail.com>
2025-02-24 16:05:17 +0530

    perf: Minify javascript using terser
    - Configure terser to remove all consoles
    - Configure terser to remove all comments
    - Reduce build javascript size for better performance'

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), compression({ algorithm: "brotliCompress" })],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
 });
