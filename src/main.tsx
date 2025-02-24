280d852d5a616be454358e43a6bbf47478e9161d
Neeraj Singh <869neerajsingh@gmail.com>
2025-02-24 16:32:00 +0530

    perf: Preconnect with firestore google apis

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import { store } from "./redux/store.js";
 import App from "./App";
@@ -10,8 +11,13 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
-      <Toaster position="top-right" />
+      <HelmetProvider>
+        <Helmet>
+          <link rel="preconnect" href="https://firestore.googleapis.com" />
+        </Helmet>
+        <App />
+        <Toaster position="top-right" />
+      </HelmetProvider>
     </React.StrictMode>
   </Provider>
 );
